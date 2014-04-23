6f724398f2e0ce99f77d2af59ffab22bdadffd34
Pimenta <matheuscscp@gmail.com>
2014-04-23 03:33:37 -0300

    Testing TCP

--- main.cpp
+++ main.cpp
@@ -5,21 +5,72 @@
  *      Author: Pimenta
  */
 
+#include <SDL.h>
+#include <SDL_error.h>
+#include <SDL_main.h>
 #include <SDL_net.h>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+void init() {
+  if (SDL_Init(0)) {
+    fprintf(stderr, "SDL_Init: %s\n", SDL_GetError());
+    exit(0);
+  }
+  
+  if (SDLNet_Init()) {
+    fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
+    exit(0);
+  }
+}
+
+void close() {
+  SDLNet_Quit();
+  SDL_Quit();
+}
 
 int main(int argc, char* argv[]) {
-  SDL_version compile_version;
-  const SDL_version *link_version=SDLNet_Linked_Version();
-  SDL_NET_VERSION(&compile_version);
-  printf("compiled with SDL_net version: %d.%d.%d\n", 
-          compile_version.major,
-          compile_version.minor,
-          compile_version.patch);
-  printf("running with SDL_net version: %d.%d.%d\n", 
-          link_version->major,
-          link_version->minor,
-          link_version->patch);
+  init();
+  
+  // create a listening TCP socket on port 8080 (server)
+  IPaddress ip;
+  TCPsocket tcpsock;
+  
+  if(SDLNet_ResolveHost(&ip,NULL,8080)==-1) {
+      printf("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+      exit(1);
+  }
+  
+  tcpsock=SDLNet_TCP_Open(&ip);
+  if(!tcpsock) {
+      printf("SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+      exit(2);
+  }
+  
+  char * headers =
+      "HTTP 1.1 200 OK\n"
+      "Content-Type: text/html;\n"
+      "Server: rosettaNaBoletta 0.1;\n"
+      "\n";
+  char* msg = "<html><body>hello world</body></html>";
+  
+  while (true) {
+    TCPsocket client = SDLNet_TCP_Accept(tcpsock);
+    if (client == nullptr)
+      continue;
+    else
+      printf("olha q maravilha\n");
+    SDLNet_TCP_Send(client, headers, strlen(headers));
+    SDLNet_TCP_Send(client, msg, strlen(msg));
+    SDLNet_TCP_Close(client);
+  }
+  
+  SDLNet_TCP_Close(tcpsock);
+  
+  close();
   
   return 0;
 }
