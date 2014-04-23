028b75a2b78fc99c8621845571516a81b71a1408
Pimenta <matheuscscp@gmail.com>
2014-04-23 01:21:55 -0300

    Adding 3rd party libs

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@
  *      Author: Pimenta
  */
 
+#include <SDL_net.h>
+#include <cstdio>
+
 int main(int argc, char* argv[]) {
+  SDL_version compile_version;
+  const SDL_version *link_version=SDLNet_Linked_Version();
+  SDL_NET_VERSION(&compile_version);
+  printf("compiled with SDL_net version: %d.%d.%d\n", 
+          compile_version.major,
+          compile_version.minor,
+          compile_version.patch);
+  printf("running with SDL_net version: %d.%d.%d\n", 
+          link_version->major,
+          link_version->minor,
+          link_version->patch);
+  
   return 0;
 }
