5f83f73353004a8a7daa31b9143f1c2c952ea63f
Felipe Rodopoulos de Oliveira <feliperodopoulos@cjr.org.br>
2014-05-06 22:02:07 -0300

    Interface web melhorada com tentativa de upload de arquivos

--- www/script.js
+++ www/script.js
@@ -6,11 +6,6 @@ function init(){
 	var ip = document.getElementById("host-ip").innerHTML = window.location.host;
 }
 
-function addButtonClicked(){
-	var action = "new-" + page.substring(7);
-	document.getElementById(action).style.display = 'block';
-}
-
 function configureBrowserRequest(xmlhttp){
 	if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
 		xmlhttp = new XMLHttpRequest();
@@ -39,69 +34,111 @@ function menuOpClicked(option){
 	xmlhttp.send();
 }
 
+function addButtonClicked(){
+	var action = "new-" + page.substring(7);
+	document.getElementById(action).style.display = 'block';
+}
+
 function addOpClicked(option){
 	var request = option.id;
 	var xmlhttp;
 	
-	xmlhttp = configureBrowserRequest(xmlhttp);
-	
+	xmlhttp = configureBrowserRequest(xmlhttp);	
 }
 
 function defineRequest(ID){
 	switch (ID){
 		case 'menuop-home':
-			changeContentHeader(false, '#fff');
-			return 'home'; 
+			changeContentHeader(false, '#fff', '');
+			return 'home.html'; 
 		break;
 		case 'menuop-user': 		
-			changeContentHeader(true, '#f00');
+			changeContentHeader(true, '#f00', '#FF5C5C');
 			return 'listUsers.html'; 
 		break;
 		case 'menuop-file': 		
-			changeContentHeader(true, '#00f');
+			changeContentHeader(true, '#00f', '#7070FF');
 			return 'listFiles.html'; 
 		break;
 		case 'menuop-host': 		
-			changeContentHeader(false, '#00a118');
+			changeContentHeader(false, '#00a118', '');
 			return 'listHosts.html'; 
 		break;
 		case 'menuop-presentation': 
-			changeContentHeader(false, '#bfb600');
+			changeContentHeader(false, '#F5E900', '');
 			return 'presentation.html'; 
 		break;
 		default: alert("Link inválido"); break;
 	}
 }
 
-function changeContentHeader(addOption, color){
+function changeContentHeader(addOption, color, button){
 	document.getElementById('content-header').style.background = color;
+	document.getElementById('add-button').style.background = button;
 	
 	if (addOption){
 		document.getElementById("add-button").style.display = 'block';
+		document.getElementById("content-header-offset").style.display = 'none';
 	} else{
 		document.getElementById("add-button").style.display = 'none';
+		document.getElementById("content-header-offset").style.display = 'block';
 	}
 }
 
 function addingUser(){
 	var username = document.getElementById("input-user").value;
 	var password = document.getElementById("input-pass").value;
-	
+	var msgArea = document.getElementById("form-message");
 	var xmlhttp;
-	page = option.id;
 	
-	xmlhttp = configureBrowserRequest(xmlhttp);
-	
-	xmlhttp.onreadystatechange = function() {
-		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-			if (xmlhttp.responseText == '1'){
-				
-			} else{
-				
+	if( !username || !password ){
+		msgArea.innerHTML = "Preencha todos os campos!";
+		msgArea.style.color = '#f00';
+	} else{
+		
+		xmlhttp = configureBrowserRequest(xmlhttp);
+		
+		xmlhttp.onreadystatechange = function() {
+			if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+				if (xmlhttp.responseText == '1'){
+					msgArea.innerHTML = "Usuário cadastrado com sucesso!";
+					msgArea.style.color = '#00ff00';
+				} else{
+					msgArea.innerHTML = "Ocorreu um erro no cadastro. Tente novamente";
+					msgArea.style.color = '#f00';
+				}
 			}
 		}
-	}
 
-	xmlhttp.open("POST", "add-user", true);
-	xmlhttp.send("user=" + username + "&pass=" + password);
+		xmlhttp.open("POST", "?add-user", true);
+		xmlhttp.send("user=" + username + "&pass=" + password);
+	}
+}
+
+function addFileInput(){
+	var plusField = document.createElement("input");
+	plusField.setAttribute('type', 'file');
+	plusField.setAttribute('name', 'files[]');
+	document.getElementById("file-inputs").appendChild(plusField);
+}
+
+function submitFilesButtonClicked(){
+	var fileSelectors = document.getElementsByName("files[]");
+	var client;
+	var formData = new FormData();
+	
+	client = configureBrowserRequest(client);
+	
+	for(var i = 0; i < fileSelectors.length; i++){
+		var fileSelector = fileSelectors[i];
+		var fileName = fileSelector.files[0].name;
+		alert(fileName);
+		
+		formData.append("upload", fileSelector.files[0]);
+		
+		client.open("post", "/upload", true);
+		client.setRequestHeader("Content-Type", "multipart/form-data");
+		client.send(formData);
+		
+	}
 }
