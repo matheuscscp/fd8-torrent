c1355e1af6b7c47a8213ee56d7f4164ac863a1c0
Felipe Rodopoulos de Oliveira <feliperodopoulos@cjr.org.br>
2014-05-08 18:10:44 -0300

    Alteracoes na pagina web com CSS e JS

--- www/script.js
+++ www/script.js
@@ -1,22 +1,80 @@
+// variavel global para indicar a pagina que o sistema se encontra
 var page = 'home';
 
-window.onload = init();
-
-function init(){
-	var ip = document.getElementById("host-ip").innerHTML = window.location.host;
-}
-
+// Funcao para inicializar a variavel de requisicao para o servidor
 function configureBrowserRequest(xmlhttp){
-	if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	if (window.XMLHttpRequest) // code for IE7+, Firefox, Chrome, Opera, Safari
 		xmlhttp = new XMLHttpRequest();
-	}
-	else { // code for IE6, IE5
+	else // code for IE6, IE5
 		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	
 	return xmlhttp;
 }
 
+function init(){
+	refreshSideInfo();
+	setInterval(function(){refreshSideInfo()}, 10000);
+}
+
+// Funcao que pergunta ao servidor o IP do host e retorna o mesmo
+function getHostIP(){
+	var client;
+	client = configureBrowserRequest(client);	
+	client.open("POST", "?R-host-ip", true);
+	client.send();
+	
+	client.onreadystatechange = function() {
+		if(client.readyState == 4 && client.status == 200)
+			return client.responseText;
+		else
+			return "-";
+	}
+	
+	return '-'; 
+}
+
+// Funcao que pergunta ao servidor o numero de hosts e retorna o mesmo
+function getNumberOfHosts(){ 
+	var client;
+	client = configureBrowserRequest(client);	
+	client.open("POST", "?R-n-hosts", true);
+	client.send();
+	
+	client.onreadystatechange = function() {
+		if(client.readyState == 4 && client.status == 200)
+			return client.responseText;
+		else
+			return "-";
+	}
+	
+	return '-'; 
+}
+
+// Funcao que pergunta ao servidor o estado do servidor e retorna-o
+function getServerStatus(){ 
+	var client;
+	client = configureBrowserRequest(client);	
+	client.open("POST", "?R-server-state", true);
+	client.send();
+	
+	client.onreadystatechange = function() {
+		if(client.readyState == 4 && client.status == 200)
+			return "On";
+		else
+			return "Off";
+	}
+
+	return 'Off'; 
+}
+
+// Funcao para atualizar as informacoes na barra lateral do sistema
+function refreshSideInfo(){
+	document.getElementById("host-ip").innerHTML = getHostIP();
+	document.getElementById("n-hosts").innerHTML = getNumberOfHosts();
+	document.getElementById("server-status").innerHTML = getServerStatus();
+}
+
+// Funcao que da funcionalidade ao menu. Ao clicar em uma opcao ele faz a 
+// requisicao da pagina correspondente e manda para a div 'content'
 function menuOpClicked(option){
 	var request = defineRequest(option.id);
 	var xmlhttp;
@@ -34,16 +92,11 @@ function menuOpClicked(option){
 	xmlhttp.send();
 }
 
+// Funcao que abre a div de cadastro de novo usuario/arquivo
 function addButtonClicked(){
 	var action = "new-" + page.substring(7);
+	alert(action);
 	document.getElementById(action).style.display = 'block';
-}
-
-function addOpClicked(option){
-	var request = option.id;
-	var xmlhttp;
-	
-	xmlhttp = configureBrowserRequest(xmlhttp);	
 }
 
 function defineRequest(ID){
@@ -89,13 +142,15 @@ function addingUser(){
 	var username = document.getElementById("input-user").value;
 	var password = document.getElementById("input-pass").value;
 	var msgArea = document.getElementById("form-message");
+	var loader = document.getElementById("loader");
 	var xmlhttp;
 	
 	if( !username || !password ){
 		msgArea.innerHTML = "Preencha todos os campos!";
 		msgArea.style.color = '#f00';
 	} else{
-		
+		loader.style.display = 'block';
+		alert("oi");
 		xmlhttp = configureBrowserRequest(xmlhttp);
 		
 		xmlhttp.onreadystatechange = function() {
@@ -112,6 +167,7 @@ function addingUser(){
 
 		xmlhttp.open("POST", "?add-user", true);
 		xmlhttp.send("user=" + username + "&pass=" + password);
+		loader.style.display = 'none';
 	}
 }
 
@@ -124,21 +180,22 @@ function addFileInput(){
 
 function submitFilesButtonClicked(){
 	var fileSelectors = document.getElementsByName("files[]");
-	var client;
+	var formMsg = document.getElementById("form-message");
 	var formData = new FormData();
+	var client;
 	
 	client = configureBrowserRequest(client);
 	
 	for(var i = 0; i < fileSelectors.length; i++){
 		var fileSelector = fileSelectors[i];
 		var fileName = fileSelector.files[0].name;
-		alert(fileName);
 		
-		formData.append("upload", fileSelector.files[0]);
-		
-		client.open("post", "/upload", true);
-		client.setRequestHeader("Content-Type", "multipart/form-data");
-		client.send(formData);
-		
+		if( fileName ){	
+			formData.append("upload", fileSelector.files[0]);
+			
+			client.open("post", "/upload", true);
+			client.setRequestHeader("Content-Type", "multipart/form-data");
+			client.send(formData);
+		}
 	}
 }
