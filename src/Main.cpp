3b3b13d79e19cb75d3c7eb0b7fa14484610991bd
Pimenta <matheuscscp@gmail.com>
2014-06-19 02:19:49 -0300

    Calling images destructors

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,32 +15,34 @@ int main(int argc, char* argv[]) {
   
   Context::init("..::fd8-torrent::..", 512, 512, "img/icon.jpg");
   
-  Context::Image bg("img/bg.jpg");
-  Context::Image start("img/start.png");
-  Context::Image stop("img/stop.png");
-  Context::Image browse("img/browse.png");
-  
-  while (!Context::quitRequested()) {
-    Context::input();
+  {
+    Context::Image bg("img/bg.jpg");
+    Context::Image start("img/start.png");
+    Context::Image stop("img/stop.png");
+    Context::Image browse("img/browse.png");
     
-    // update
-    if (start.leftClicked() && !System::changing()) {
-      if (!System::start())
-        System::stop();
+    while (!Context::quitRequested()) {
+      Context::input();
+      
+      // update
+      if (start.leftClicked() && !System::changing()) {
+        if (!System::start())
+          System::stop();
+      }
+      if (browse.leftClicked() && System::running())
+        helpers::openBrowser();
+      
+      // render
+      bg.render(0, 0);
+      if (System::running()) {
+        stop.render(155, 300);
+        browse.render(205, 102);
+      }
+      else
+        start.render(155, 300);
+      
+      Context::render();
     }
-    if (browse.leftClicked() && System::running())
-      helpers::openBrowser();
-    
-    // render
-    bg.render(0, 0);
-    if (System::running()) {
-      stop.render(155, 300);
-      browse.render(205, 102);
-    }
-    else
-      start.render(155, 300);
-    
-    Context::render();
   }
   
   Context::closeWindow();
