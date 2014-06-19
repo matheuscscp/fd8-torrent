8564bedb648e81521d9830ab7d00a1202b03b300
Pimenta <matheuscscp@gmail.com>
2014-06-19 17:50:47 -0300

    Adding protocol file

---
+++ src/FD8Protocol.hpp
@@ -0,0 +1,19 @@
+/*
+ * FD8Protocol.hpp
+ *
+ *  Created on: Jun 19, 2014
+ *      Author: Pimenta
+ */
+
+#ifndef FD8PROTOCOL_HPP_
+#define FD8PROTOCOL_HPP_
+
+namespace fd8protocol {
+
+enum {
+  MTYPE_GETUSERS = 0
+};
+
+} // namespace fd8protocol
+
+#endif /* FD8PROTOCOL_HPP_ */
