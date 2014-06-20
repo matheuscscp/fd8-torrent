48ba5a2b76a3e12268a85bd8b8b03a3109c6581d
Pimenta <matheuscscp@gmail.com>
2014-06-19 21:51:42 -0300

    Answering fd8 protocol messages

---
+++ src/System_answerRequests.cpp
@@ -0,0 +1,38 @@
+/*
+ * System_answerRequests.cpp
+ *
+ *  Created on: Jun 19, 2014
+ *      Author: Pimenta
+ */
+
+// this
+#include "System.hpp"
+
+// local
+#include "Network.hpp"
+#include "FD8Protocol.hpp"
+
+using namespace network;
+
+// static variables
+static TCPConnection* peer;
+
+void System::answerRequests() {
+  peer = mainTCPServer.accept();
+  if (peer == nullptr)
+    return;
+  
+  char request = peer->recv<char>();
+  switch (request) {
+    case fd8protocol::MTYPE_GETUSERS:
+      peer->send(uint32_t(users.size()));
+      for (auto& kv : users) {
+        peer->send(kv.first);
+        peer->send(kv.second.name);
+      }
+      break;
+      
+    default:
+      break;
+  }
+}
