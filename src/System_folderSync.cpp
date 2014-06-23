b6041660b1efc33a1b3716b4e840065b267ccdc2
Pimenta <matheuscscp@gmail.com>
2014-06-23 02:39:30 -0300

    Adding definitions of folder sync methods

--- src/System_folderSync.cpp
+++ src/System_folderSync.cpp
@@ -5,6 +5,22 @@
  *      Author: Pimenta
  */
 
+// this
+#include "System.hpp"
 
+using namespace std;
+using namespace concurrency;
 
+void System::createFolder(const string& fullPath) {
+  Thread([]() {
+    
+  }).start();
+}
 
+void System::updateFolder(const string& fullPath, const string& newName) {
+  
+}
+
+void System::deleteFolder(const string& fullPath) {
+  
+}
