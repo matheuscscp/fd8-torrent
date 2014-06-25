9a06cc33cc91df81059e04924c8a675d35900223
Pimenta <matheuscscp@gmail.com>
2014-06-25 06:38:09 -0300

    Removing old files implemented

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -198,19 +198,19 @@ FileSystem::Folder* FileSystem::Folder::findFirstBottomUp_(const string& subPath
   return parentFolder->second.findFirstBottomUp_(brokenPath.second, foundPath);
 }
 
-void FileSystem::init(uint32_t localIP) {//FIXME
+void FileSystem::init(uint32_t localIP) {
   rootFolder.subfolders.clear();
   rootFolder.files.clear();
-#ifdef _WIN32
-  //system("rmdir /Q /S www\files");
- // system("mkdir www\files");
-#else
-  //system("rm -rf www/files");
-  //system("mkdir www/files");
-#endif
   FileSystem::localIP = localIP;
   nextID = 1;
   storedFiles.clear();
+#ifdef _WIN32
+  system("rmdir /Q /S www\\files");
+  system("mkdir www\\files");
+#else
+  system("rm -rf www/files");
+  system("mkdir www/files");
+#endif
 }
 
 ByteQueue FileSystem::serialize() {
