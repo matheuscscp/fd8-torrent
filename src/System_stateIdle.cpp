90cd55a68eb1e160aff4bce715587c9b5fc7f5ee
Pimenta <matheuscscp@gmail.com>
2014-06-26 20:47:38 -0300

    Fixing bug

--- src/System_stateIdle.cpp
+++ src/System_stateIdle.cpp
@@ -14,6 +14,7 @@
 using namespace concurrency;
 
 void System::changeToIdle() {
+  FileSystem::init(localAddress.ip);
   requestSystemState();
   httpThread = Thread([this]() {
     while (state == newState) {
