3754c5f350012db110fc54ec286e05f7cc47e960
Pimenta <matheuscscp@gmail.com>
2014-07-02 05:47:06 -0300

    Organizing code

--- src/System_detectFailure.cpp
+++ src/System_detectFailure.cpp
@@ -32,6 +32,7 @@ void System::detectFailure() {
 }
 
 void System::recoverFromFailure() {
+  // check who is the designated peer
   set<uint32_t> peers;
   uint32_t designatedPeer = 0;
   for (auto& kv : users) {
@@ -39,26 +40,30 @@ void System::recoverFromFailure() {
     if (kv.first > designatedPeer)
       designatedPeer = kv.first;
   }
-  if (designatedPeer == localAddress.ip) {
-    FileSystem::initTmpFileSystem();
-    list<FileSystem::Command*> cmds = FileSystem::calculateDuplications(peers);
-    list<FileSystem::Command*> balCmds = FileSystem::calculateBalancing(peers);
-    for (auto& cmd : balCmds)
-      cmds.push_back(cmd);
-    FileSystem::eliminateIntersections(cmds);
-    ByteQueue data = FileSystem::Command::serialize(cmds);
-    for (auto& kv : users) {
-      if (kv.first == localAddress.ip)
-        continue;
-      TCPConnection conn(Address(kv.first, Address("", TCPUDP_MAIN).port));
-      conn.send(char(MTYPE_COMMANDS));
-      conn.send(uint32_t(data.size()));
-      conn.send(data);
-      conn.recv<char>();
-    }
-    send_files(cmds);
-    FileSystem::processCommands(cmds);
-    for (auto& cmd : cmds)
-      delete cmd;
+  
+  // if the designated peer is not this one
+  if (designatedPeer != localAddress.ip)
+    return;
+  
+  // recover
+  FileSystem::initTmpFileSystem();
+  list<FileSystem::Command*> cmds = FileSystem::calculateDuplications(peers);
+  list<FileSystem::Command*> balCmds = FileSystem::calculateBalancing(peers);
+  for (auto& cmd : balCmds)
+    cmds.push_back(cmd);
+  FileSystem::eliminateIntersections(cmds);
+  ByteQueue data = FileSystem::Command::serialize(cmds);
+  for (auto& kv : users) {
+    if (kv.first == localAddress.ip)
+      continue;
+    TCPConnection conn(Address(kv.first, Address("", TCPUDP_MAIN).port));
+    conn.send(char(MTYPE_COMMANDS));
+    conn.send(uint32_t(data.size()));
+    conn.send(data);
+    conn.recv<char>();
   }
+  send_files(cmds);
+  FileSystem::processCommands(cmds);
+  for (auto& cmd : cmds)
+    delete cmd;
 }
